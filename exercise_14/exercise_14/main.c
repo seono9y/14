2efdf62573f53d6b5db65020cfbcaff9de323df9
seono9y <144989211+seono9y@users.noreply.github.com>
2023-12-10 18:57:14 +0900

    03

--- exercise_14/exercise_14/main.c
+++ exercise_14/exercise_14/main.c
@@ -14,19 +14,28 @@ struct Book{
 };
 
 int main(int argc, char *argv[]) {
-    struct Book *p;
-
-    p = (struct Book*)malloc(2*sizeof(struct Book));
-    if(p==NULL){
-        printf("¸Þ¸ð¸® ÇÒ´ç ¿À·ù\n");
-        return;
-    }
-    p -> number = 1;
-    strcpy(p->title, "C Programing");
-
-    (p+1) -> number = 2;
-    strcpy((p+1)->title, "Electronics");
-
-    free(p);
+    int i;
+    struct Book **bookshelf;
+    
+    bookshelf = (struct Book**)malloc( 3*sizeof(struct Book*)); //구조체 포인터 변수 3개 할당
+    for(i=0;i<3;i++)
+        bookshelf[i] = (struct Book *)malloc(10*sizeof(struct Book));//구조체 10개 할당
+    
+    bookshelf[1][3].number = 5;
+    strcpy(bookshelf[1][3].title, "C++Programming");
+    
+    (bookshelf[2]+4)->number = 3;
+    strcpy((bookshelf[2]+4)->title,"Communications Theory") ;
+    
+    printf("book(1,3) : %i, %s\n", (bookshelf[1]+3)->number, (bookshelf[1]+3)->title);
+    printf("book(2,4) : %i, %s\n", bookshelf[2][4].number,bookshelf[2][4].title);
+    
+    //구조체 포인터 3개가 각각 가리키는 주소 해제
+    for(i=0;i<3;i++)
+        free(bookshelf[i]);
+    //구조체 이중 포인터가 가리키는 주소 해제
+    free(bookshelf);
+    
+    
     return 0;
 }
